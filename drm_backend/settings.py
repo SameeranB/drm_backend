1c0e10fbef842984d774b05275c604a559fbf9ab
SameeranB <sameeranbandishti93@gmail.com>
2020-09-06 18:16:37 +0530

    Configure server to run in development by default

--- drm_backend/settings.py
+++ drm_backend/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "TempSecretKey")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = not bool(int(os.environ.get("PROD"), "0"))
+DEBUG = not bool(int(os.environ.get("PROD", "0")))
 DEBUG_PROPAGATE_EXCEPTIONS = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.environ.get("PROD_HOSTNAME_1"), os.environ.get("PROD_HOSTNAME_2")]
@@ -146,7 +146,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-if not bool(int(os.environ.get("PROD"), "0")):
+if not bool(int(os.environ.get("PROD", "0"))):
     STATIC_ROOT = '/vol/web/static'
 else:
     STATIC_ROOT = 'staticfiles'
