0e05499ee5ed9edd46c5a876e50804b2deea6088
SameeranB <sameeranbandishti93@gmail.com>
2020-09-08 17:54:34 +0530

    Implement OAuth with Google

--- users_module/views.py
+++ users_module/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from rest_auth.registration.views import SocialLoginView
+
 
 # Create your views here.
+
+class GoogleLogin(SocialLoginView):
+    adapter_class = GoogleOAuth2Adapter
